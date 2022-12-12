ef2b422a24f6cd8a7871c9b8c3545252ca08f9f3
nguyenhainam24082000 <nghainam2000@gmail.com>
2022-12-13 03:04:08 +0700

    update chunkSizeWarningLimit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import autoprefixer from 'autoprefixer';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 2048,
+    chunkSizeWarningLimit: 10000,
     sourcemap: true,
   },
   plugins: [react(), tsconfigPaths()],
